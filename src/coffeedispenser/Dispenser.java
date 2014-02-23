70275debe4c9f8e5a9f175805b0f19f02dcd5e3e
Gaurang <gaurangkher@yahoo.com>
2014-02-23 01:08:54 -0600

    test

--- src/coffeedispenser/Dispenser.java
+++ src/coffeedispenser/Dispenser.java
@@ -7,13 +7,16 @@ public class Dispenser {
 
 	HashMap ingredients = new HashMap();
 	ArrayList beverages = new ArrayList();
+	
 	Dispenser(String filename, String[] beverages) {
 		
 	}
 	
 	public void display() {}
 	
-	public Beverage getBeverage() { return new Mocha(); }
+	public Beverage getBeverage() { 
+		ClassLoader loader = new ClassLoader();
+		return new Mocha(); }
 	
 	public void reStock() {}
 	
