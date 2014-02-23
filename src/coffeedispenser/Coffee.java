cbd9b714eda25f0abcbbf185df247a08a5d6d1df
regnaresh <regnaresh@gmail.com>
2014-02-23 00:57:01 -0600

    test

---
+++ src/coffeedispenser/Coffee.java
@@ -0,0 +1,23 @@
+package coffeedispenser;
+
+import java.util.HashMap;
+
+public class Coffee extends Beverage{
+
+	String coffee = new String("CoffeeBean");
+	String sugar = new String("Sugar");
+	String cream = new String("Cream");
+
+	public Coffee(HashMap ingredient) 
+	{
+		//if(ingredient.get(coffee).)
+	}
+	
+	@Override
+	public void calculateCost() {
+		// TODO Auto-generated method stub
+		
+		//il.getPrice()
+	}
+
+}
--- src/coffeedispenser/CoffeeBean.java
+++ src/coffeedispenser/CoffeeBean.java
@@ -2,9 +2,22 @@ package coffeedispenser;
 
 public class CoffeeBean extends Ingredient{
 
-	public CoffeeBean(float price, int initial_quantity)
+	private static CoffeeBean coffeeBean =  null;
+	
+	protected CoffeeBean(float price, int initial_quantity)
 	{
 		setPrice(price);
 		setQuantity(initial_quantity);
+	}
+
+	public static CoffeeBean getInstance(float price, int initial_quantity)
+	{
+		if(coffeeBean == null)
+			return new CoffeeBean(price, initial_quantity); 
+		
+		return coffeeBean;
+	}
+	public CoffeeBean() {
+		// TODO Auto-generated constructor stub
 	}
 }
